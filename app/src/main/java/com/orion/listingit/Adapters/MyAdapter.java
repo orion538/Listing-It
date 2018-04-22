0df04b06479fac26822e1dfa0f4c670d4bb479f2
Orion <hans-jurgenbakkenes@hotmail.com>
2018-04-22 03:20:13 +0200

    drag & drop
    Added basic drag & drop to the list

--- app/src/main/java/com/orion/listingit/Adapters/MyAdapter.java
+++ app/src/main/java/com/orion/listingit/Adapters/MyAdapter.java
@@ -1,29 +1,48 @@
 package com.orion.listingit.Adapters;
 
+import android.support.v4.view.MotionEventCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.orion.listingit.Interfaces.IItemTouchHelperAdapter;
+import com.orion.listingit.Interfaces.OnStartDragListener;
 import com.orion.listingit.R;
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
-    private String[] mDataset;
+import java.util.Collections;
+import java.util.List;
+
+public class MyAdapter
+        extends RecyclerView.Adapter<MyAdapter.ViewHolder>
+        implements IItemTouchHelperAdapter {
+    private OnStartDragListener mDragStartListener;
+    private List<String> mDataset;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
+        public FrameLayout mFrameLayout;
         public TextView mTextView;
-        public ViewHolder(TextView v) {
+        public ImageView mImageView;
+        public ViewHolder(FrameLayout v, ImageView i, TextView t) {
             super(v);
-            mTextView = v;
+            mFrameLayout = v;
+            mTextView = t;
+            mImageView = i;
         }
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public MyAdapter(String[] myDataset) {
+    public MyAdapter(OnStartDragListener dragStartListener,
+                     List<String> myDataset) {
+        mDragStartListener = dragStartListener;
         mDataset = myDataset;
     }
 
@@ -32,24 +51,55 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
                                                    int viewType) {
         // create a new view
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+        FrameLayout v = (FrameLayout) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.text_row_item, parent, false);
-        ViewHolder vh = new ViewHolder(v);
+        TextView text = v.findViewById(R.id.text);
+        ImageView handle = v.findViewById(R.id.handle);
+        ViewHolder vh = new ViewHolder(v, handle, text);
         return vh;
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(final ViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.mTextView.setText(mDataset[position]);
+        holder.mTextView.setText(mDataset.get(position));
 
+        holder.mImageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    mDragStartListener.onStartDrag(holder);
+                }
+                return false;
+            }
+        });
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.length;
+        return mDataset.size();
+    }
+
+    @Override
+    public void onItemMove(int fromPosition, int toPosition) {
+        if (fromPosition < toPosition) {
+            for (int i = fromPosition; i < toPosition; i++) {
+                Collections.swap(mDataset, i, i + 1);
+            }
+        } else {
+            for (int i = fromPosition; i > toPosition; i--) {
+                Collections.swap(mDataset, i, i - 1);
+            }
+        }
+        notifyItemMoved(fromPosition, toPosition);
+    }
+
+    @Override
+    public void onItemDismiss(int position) {
+        mDataset.remove(position);
+        notifyItemRemoved(position);
     }
 }
--- app/src/main/java/com/orion/listingit/ListDetailActivity.java
+++ app/src/main/java/com/orion/listingit/ListDetailActivity.java
@@ -4,24 +4,34 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.orion.listingit.Adapters.MyAdapter;
+import com.orion.listingit.Helpers.SimpleItemTouchHelperCallback;
+import com.orion.listingit.Interfaces.OnStartDragListener;
 
-public class ListDetailActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDetailActivity
+        extends AppCompatActivity
+        implements OnStartDragListener {
 
     private static final int DATASET_COUNT = 60;
 
     private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+    private MyAdapter mAdapter;
+    private LinearLayoutManager mLayoutManager;
+    private ItemTouchHelper mItemTouchHelper;
 
-    protected String[] mDataset;
+    protected List<String> mDataset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +49,22 @@ public class ListDetailActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(mRecyclerView.getContext(),
+                DividerItemDecoration.VERTICAL);
+        mRecyclerView.addItemDecoration(dividerItemDecoration);
+
         // Initialize dataset, this data would usually come from a local content provider or
         // remote server.
         initDataset();
 
-        // specify an adapter (see also next example)
-        mAdapter = new MyAdapter(mDataset);
+        // specify an adapter
+        mAdapter = new MyAdapter(this, mDataset);
         mRecyclerView.setAdapter(mAdapter);
+
+        ItemTouchHelper.Callback callback =
+                new SimpleItemTouchHelperCallback(mAdapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -83,9 +102,14 @@ public class ListDetailActivity extends AppCompatActivity {
      * from a local content provider or remote server.
      */
     private void initDataset() {
-        mDataset = new String[DATASET_COUNT];
+        mDataset = new ArrayList<String>();
         for (int i = 0; i < DATASET_COUNT; i++) {
-            mDataset[i] = "This is element #" + i;
+            mDataset.add("This is element #" + i);
         }
+    }
+
+    @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        mItemTouchHelper.startDrag(viewHolder);
     }
 }
